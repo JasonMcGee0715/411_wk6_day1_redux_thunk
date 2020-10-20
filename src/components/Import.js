2a89b796dc88a2bd9330204b018fb122959c65ec
JasonMcGee0715 <jdm0715@gmail.com>
2020-10-19 22:06:25 -0500

    first commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import {
+    Button,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Container,
+} from '@material-ui/core';
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <div>
+        <Button variant='contained' color='primary'>Import</Button>
+        <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="right">Make</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+        </div>
     )
 }
 
